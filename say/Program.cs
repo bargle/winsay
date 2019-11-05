ca00e3b96a291503c5375bfbafbe7072cd43426c
David Koenig <david@gamedev.engineer>
2019-11-05 09:31:15 -0600

    Add -o option to output to wave.

--- say/Program.cs
+++ say/Program.cs
@@ -11,23 +11,26 @@ namespace say
 {
     class Options
     {
-        [Option('m', "message", Required = false, HelpText = "Text to Speak")]
+        [Option( 'm', "message", Required = false, HelpText = "Text to Speak" )]
         public string Message { get; set; }
 
-        [Option('v', "voice", Required = false, HelpText = "TTS voice to use")]
+        [Option( 'v', "voice", Required = false, HelpText = "TTS voice to use" )]
         public string TTSVoice { get; set; }
 
-        [Option('s', "speed", Required = false, HelpText = "Speed for TTS voice")]
+        [Option( 's', "speed", Required = false, HelpText = "Speed for TTS voice" )]
         public int TTSSpeed { get; set; }
 
-        [Option('g', "gender", Required = false, HelpText = "Select TTS voice by gender")]
+        [Option( 'g', "gender", Required = false, HelpText = "Select TTS voice by gender" )]
         public string TTSGender { get; set; }
 
-        [Option('i', "volume", Required = false, HelpText = "Volume of TTS voice")]
+        [Option( 'i', "volume", Required = false, HelpText = "Volume of TTS voice" )]
         public int TTSVolume { get; set; }
 
-        [Option('l', "lsvoices", Required = false, HelpText = "show list of installed TTS voices")]
+        [Option( 'l', "lsvoices", Required = false, HelpText = "show list of installed TTS voices" )]
         public bool ListVoices { get; set; }
+
+        [Option( 'o', "output", Required = false, HelpText = "Output to wave file" )]
+        public string TTSFile { get; set; }
 
         [HelpOption]
         public string GetUsage()
@@ -39,9 +42,9 @@ namespace say
                 AdditionalNewLineAfterOption = true,
                 AddDashesToOption = true
             };
-            help.AddPreOptionsLine("all rights reserved");
-            help.AddPreOptionsLine("Usage: [Message] <other options>");
-            help.AddOptions(this);
+            help.AddPreOptionsLine( "all rights reserved" );
+            help.AddPreOptionsLine( "Usage: [Message] <other options>" );
+            help.AddOptions( this );
             return help;
         }
     }
@@ -52,41 +55,48 @@ namespace say
         private static InstalledVoice[] voiceList = synth.GetInstalledVoices().ToArray();
 
         public static Options options = new Options();
-        public static void Main(String[] args)
+        public static void Main( String[] args )
         {
 
-            if (CommandLine.Parser.Default.ParseArguments(args, options)) 
+            if ( CommandLine.Parser.Default.ParseArguments( args, options ) )
             {
-                if (options.ListVoices)
+                if ( options.ListVoices )
                 {
                     listVoices();
                 }
-                if (options.TTSGender == "male" || options.TTSGender == "female")
+
+                if ( options.TTSGender == "male" || options.TTSGender == "female" )
                 {
                     VoiceGender genderMale = VoiceGender.Male;
                     VoiceGender genderFemale = VoiceGender.Female;
-                    if (options.TTSGender == "female"){
-                        synth.SelectVoiceByHints(genderFemale);
+                    if ( options.TTSGender == "female" )
+                    {
+                        synth.SelectVoiceByHints( genderFemale );
                     }
-                    else if(options.TTSGender == "male"){
-                        synth.SelectVoiceByHints(genderMale);
+                    else if ( options.TTSGender == "male" )
+                    {
+                        synth.SelectVoiceByHints( genderMale );
                     }
                 }
-                if (options.TTSSpeed <= 10 && options.TTSSpeed >= -10)
+                if ( options.TTSSpeed <= 10 && options.TTSSpeed >= -10 )
                 {
                     synth.Rate = options.TTSSpeed;
                 }
-                if (options.TTSVolume > 0 && options.TTSVolume <= 100)
+                if ( options.TTSVolume > 0 && options.TTSVolume <= 100 )
                 {
                     synth.Volume = options.TTSVolume;
                 }
-                if (options.TTSVoice != null)
+                if ( options.TTSVoice != null )
                 {
-                    synth.SelectVoice(parseVoice(options.TTSVoice));
+                    synth.SelectVoice( parseVoice( options.TTSVoice ) );
                 }
-                if (options.Message != null)
+                if ( options.TTSFile.Length > 0 )
                 {
-                    synth.Speak(parseMessage(options.Message));
+                    synth.SetOutputToWaveFile( options.TTSFile );
+                }
+                if ( options.Message != null )
+                {
+                    synth.Speak( parseMessage( options.Message ) );
                 }
             }
         }
@@ -94,31 +104,31 @@ namespace say
         public static void listVoices()
         {
             int i = 0;
-            for (i = 0; i < voiceList.Length; i++)
+            for ( i = 0; i < voiceList.Length; i++ )
             {
-                Console.Out.WriteLine(voiceList[i].VoiceInfo.Name);
+                Console.Out.WriteLine( voiceList[i].VoiceInfo.Name );
             }
         }
 
-        public static string parseVoice(string input)
+        public static string parseVoice( string input )
         {
             char underscore = '_';
             char space = ' ';
             String properVoiceName = input.Replace(underscore, space);
 
             int i = 0;
-            for (i = 0; i < voiceList.Length; i++)
+            for ( i = 0; i < voiceList.Length; i++ )
             {
-                if (properVoiceName == voiceList[i].VoiceInfo.Name)
+                if ( properVoiceName == voiceList[i].VoiceInfo.Name )
                 {
-                    Console.Out.WriteLine(properVoiceName);
+                    Console.Out.WriteLine( properVoiceName );
                     return properVoiceName;
                 }
             }
             return null;
         }
 
-        public static string parseMessage(string input)
+        public static string parseMessage( string input )
         {
             char underscore = '_';
             char space = ' ';
